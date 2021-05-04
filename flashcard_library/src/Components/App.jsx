18e4c42312c730c6077c4015bc9c3ec3765e8de6
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-03 16:30:33 -0800

    Replaced files.

---
+++ flashcard_library/src/Components/App.jsx
@@ -0,0 +1,31 @@
+
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+
+import "./App.css";
+
+class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div>Hello!</div>
+
+    );
+  }
+}
+
+App.defaultProps = {
+
+}
+
+
+export default App;
+          
