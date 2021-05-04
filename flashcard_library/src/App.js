3b341f1e88906325ca8cfbc966405a41cb69bfb8
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-03 20:11:40 -0800

    Re-installed node.js and react due to errors. Added navbar using react bootstrap and icons using react-icons

---
+++ flashcard_library/src/App.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import {Button} from 'react-bootstrap'
+import './App.css';
+import NavbarPage from './NavBar';
+
+class  App extends Component {
+
+
+
+
+
+
+  render(){
+  return (
+    <div className="App">
+      <NavbarPage></NavbarPage>
+      <h1>Hello!</h1>
+    </div>
+  );
+    }
+}
+
+export default App;
